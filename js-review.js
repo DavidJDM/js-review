39a5a1fc00499d4397ff12cb1046303006db22fc
David Kovalevich <kovalevichdavid@yahoo.com>
2019-01-14 04:06:47 -0500

    FizzBuzz function added

--- js-review.js
+++ js-review.js
@@ -1,1 +1,23 @@
 console.log("Introduction to JavaScript");
+FizzBuzz();
+
+
+function FizzBuzz() {
+    var i;
+    for(i = 1; i < 101; i++) {
+        if(((i % 3) == 0) && ((i % 5)) != 0) {
+            console.log("Fizz");
+        }
+
+        else if(((i % 5) == 0) && ((i % 3)) != 0) {
+            console.log("Buzz");
+        }
+
+        else if(((i % 3) == 0) && ((i % 5) == 0)) {
+            console.log("FizzBuzz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
